c3998bb62edf92ae72136b3e0a32935fc4eba19b
Dhruv Vira <dhruvvira007@gmail.com>
2026-04-22 17:25:50 +0530

    refactor: Update CORS settings in main.py in preparation for deployment

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://127.0.0.1:8000"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
